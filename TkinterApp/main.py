64329de13a8fea5844dbcea1941c926606e393b4
takunology <m15025@g.metro-cit.ac.jp>
2019-07-14 13:05:31 +0900

    Tkinter の修正

--- TkinterApp/main.py
+++ TkinterApp/main.py
@@ -18,11 +18,12 @@ MainWindow.maxsize(1000,1000)
 MainWindow.resizable(1,1)
 
 # ラベルの作成
-Label1 = View.Label(MainWindow, text="テストラベル")
+Label1 = View.Label(MainWindow, text="好きな文字を入力してくださいな")
 # ラベル内のフォント指定
 font1 = font.Font(family="Helvetica", size=20, weight="bold")
 # ラベルの座標指定
-Label1.place(x=100, y=20)
+#Label1.place(x=50, y=10)
+Label1.pack()
 
 # 進捗度を表示するステータスバー
 Statusbar = View.Label(MainWindow, text="ステータスが表示されます", borderwidth=2, relief="groove")
@@ -31,11 +32,18 @@ Statusbar.pack(side=View.BOTTOM, fill=View.X)
 
 # ボタンを使う
 def On_Click():
-    text = "クリックされたよ"
+    # ボタンのクリックイベントハンドラ
+    text = Textbox1.get()
     MessageBox.showinfo("ボタンクリック", text)
-
+    Statusbar.set("完了")
+# ボタンの引数に command を指定してトリガを設定する
 Button1 = View.Button(MainWindow, text="クリック", command=On_Click)
-Button1.place(x=180, y=20)
+Button1.place(x=400, y=100)
+
+# テキストボックス
+Textbox1 = View.Entry(width=30)
+Textbox1.place(x=200, y=100)
+#Textbox1.pack()
 
 # ウィンドウ GUI を動かすための処理
 MainWindow.mainloop()
